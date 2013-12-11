12445d925b7619d78c2d462beb7e7b14b0dae6c6
Morgan Delahaye-Prat <mdp@arjel.fr>
2013-12-11 15:08:21 +0100

    HTMLString python 3 and 2 support.

--- flask_triangle/helpers/python3.py
+++ flask_triangle/helpers/python3.py
@@ -3,7 +3,7 @@
     helpers.python3
     ---------------
 
-    An helper to facilitate the transition to Python 3.
+    A helper to facilitate the Python3 support.
 
     :copyright: (c) 2013 by Morgan Delahaye-Prat.
     :license: BSD, see LICENSE for more details.
@@ -12,13 +12,7 @@
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
-
 import sys
 
 
-class UnicodeMixin(object):
-    # A workaround for an easier 2to3 migration.
-    if sys.version_info > (3, 0):
-        __str__ = lambda x: x.__unicode__()
-    else:
-        __str__ = lambda x: unicode(x).encode('utf8')
+PY3 = sys.version_info > (3, 0)
--- tests/test_helpers/html.py
+++ tests/test_helpers/html.py
@@ -9,25 +9,30 @@
 
 
 from __future__ import absolute_import
-from nose.tools import assert_true, assert_equal
+from __future__ import unicode_literals
 
-from flask_triangle.helpers import HTMLString
+from nose.tools import assert_true, assert_equal
+from nose.plugins.skip import SkipTest
+
+from flask_triangle.helpers import PY3, HTMLString
 
 
 class TestHtml(object):
 
     def setup(self):
-
-        self.html = HTMLString(u'hello')
+        self.html = HTMLString('hello')
 
     def test_0(self):
-        """An HTMLString object is an unicode subtype"""
-        assert_true(isinstance(self.html, unicode))
+        """An HTMLString object is of a string type."""
+        if PY3:
+            assert_true(isinstance(self.html, str))
+        else:
+            assert_true(isinstance(self.html, unicode))
 
     def test_1(self):
-        """An HTMLString object is __html__."""
+        """An HTMLString object has a __html__ method."""
         assert_true(hasattr(self.html, '__html__'))
 
     def test_2(self):
-        """The __html__ method return the unicode string."""
+        """The __html__ method returns the string."""
         assert_equal(self.html.__html__(), self.html)
