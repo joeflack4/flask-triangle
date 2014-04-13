508b75b24dffe7ebdd77693bafbb281489b10c5a
Morgan Delahaye-Prat <mdp@m-del.net>
2014-04-13 11:55:41 -0400

    Removing of the old test infrastructure.

---
+++ tests/tests.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+"""
+    tests
+    -----
+
+    Entry-point of the testsuite.
+
+    :copyright: (c) 2013 by Morgan Delahaye-Prat.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import with_statement
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+# testenv setup
+
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+
+# import all the tests here
+
+
+if __name__ == '__main__':
+
+    unittest.main()
